e0412e0d60b1afd18d18b282471088cc1eab2696
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-23 16:32:06 -0500

    indented subprograms for easy readability

--- project3.s
+++ project3.s
@@ -22,15 +22,15 @@ li $v0, 10 # exit program syscall
 syscall
 
 sub_a: # subprogram to process entire input into substrings
-#################################################################
-# sub_a parses the input string and prints out the integers and error messages one by one, with them separated by single comma
-# input used: address of input string from stack
-# temporary registers used: $t0, $t1
-# outputs: none
-#
-# called by main
-# calls sub_b
-################################################################
+    #################################################################
+    # sub_a parses the input string and prints out the integers and error messages one by one, with them separated by single comma
+    # input used: address of input string from stack
+    # temporary registers used: $t0, $t1
+    # outputs: none
+    #
+    # called by main
+    # calls sub_b
+    ################################################################
     lw $t0,0($sp) # $t0 contains the address of the string
     add $fp,$sp,$zero # store frame information
 
@@ -73,97 +73,97 @@ sub_a: # subprogram to process entire input into substrings
 
 
 sub_b: # subprogram to process each substring
-#################################################################
-# comment
-#
-# inputs used: 0th word: address of first valid character in string from stack
-# outputs used:
-#           stack: (4 words)
-#           first word: whether string is invalid (0) or not (non-zero)
-#           second word: the convert_string_to_decimal value of string, if valid
-#           third word: unsigned number of valid chars
-#           fourth word: indentation to last char used, stores -1 if end of string
-#       
-# registers used: $a0,$t0,$t1,$t2,$t3,$t4,$t5
-#
-#
-# called by sub_a
-# calls none
-##################################################################
+    #################################################################
+    # comment
+    #
+    # inputs used: 0th word: address of first valid character in string from stack
+    # outputs used:
+    #           stack: (4 words)
+    #           first word: whether string is invalid (0) or not (non-zero)
+    #           second word: the convert_string_to_decimal value of string, if valid
+    #           third word: unsigned number of valid chars
+    #           fourth word: indentation to last char used, stores -1 if end of string
+    #       
+    # registers used: $a0,$t0,$t1,$t2,$t3,$t4,$t5
+    #
+    #
+    # called by sub_a
+    # calls none
+    ##################################################################
 
-li $t0,0 # initialized to invalid - holds whether string is invalid (0) or not (non-zero)
-li $t1,0 # initialized to 0 - holds running sum
-li $t2,0 # will hold how many valid characters found
-li $t3,0 # will hold 1 if spaces found after first non-space char
-lw $a0,0($sp) # load address stored in position 1 stack
+    li $t0,0 # initialized to invalid - holds whether string is invalid (0) or not (non-zero)
+    li $t1,0 # initialized to 0 - holds running sum
+    li $t2,0 # will hold how many valid characters found
+    li $t3,0 # will hold 1 if spaces found after first non-space char
+    lw $a0,0($sp) # load address stored in position 1 stack
 
-sub_b_loop:
-    lb $t5,0($a0) # load character at this of string into $t5
-    beq $t5,$zero,end_of_string # when null char is read, go to end_of_string
-    li $t4,10 # holds enter ascii character
-    beq $t5,$t4,end_of_string # when enter char is read in case less than 1000 chars read and the user clicks enter
-    addi $a0,$a0,1 # increment the address in $a0 by one to move onto next character in the next loop
+    sub_b_loop:
+        lb $t5,0($a0) # load character at this of string into $t5
+        beq $t5,$zero,end_of_string # when null char is read, go to end_of_string
+        li $t4,10 # holds enter ascii character
+        beq $t5,$t4,end_of_string # when enter char is read in case less than 1000 chars read and the user clicks enter
+        addi $a0,$a0,1 # increment the address in $a0 by one to move onto next character in the next loop
 
-    check_0_to_9:
-        slti $t4,$t5,48 # the string char in $t5 should be greater than or equal to '0' char i.e. $t4 should be 0
-        bne $t4,$zero,check_a_to_p # if $t4 not 0, do the next check
-        slti $t4,$t5,58 # check if character <= ascii code for 9 # the string char in $t5 should be less than or equal to '9' char i.e. $t4 should be 1
-        beq $t4,$zero,check_a_to_p # if $t4 0 instead of 1, do the next check
+        check_0_to_9:
+            slti $t4,$t5,48 # the string char in $t5 should be greater than or equal to '0' char i.e. $t4 should be 0
+            bne $t4,$zero,check_a_to_p # if $t4 not 0, do the next check
+            slti $t4,$t5,58 # check if character <= ascii code for 9 # the string char in $t5 should be less than or equal to '9' char i.e. $t4 should be 1
+            beq $t4,$zero,check_a_to_p # if $t4 0 instead of 1, do the next check
 
-        addi $t5,$t5,-48 # convert ascii value to integer (0-9 ascii: 48-57)
-        j add_to_running_sum # j to segment of loop that adds char value to value of $v1, the running sum
+            addi $t5,$t5,-48 # convert ascii value to integer (0-9 ascii: 48-57)
+            j add_to_running_sum # j to segment of loop that adds char value to value of $v1, the running sum
 
-    check_a_to_p:
-        slti $t4,$t5,97 # the string char in $t5 should be greater than or equal to 'a' char i.e. $t4 should be 0
-        bne $t4,$zero,check_A_to_P # if $t4 not 0, do the next check
-        slti $t4,$t5,113 # check if character <= ascii code for 'p' # the string char in $t5 should be less than or equal to 'p' char i.e. $t4 should be 1
-        beq $t4,$zero,check_A_to_P # if $t4 0 instead of 1, do the next check
+        check_a_to_p:
+            slti $t4,$t5,97 # the string char in $t5 should be greater than or equal to 'a' char i.e. $t4 should be 0
+            bne $t4,$zero,check_A_to_P # if $t4 not 0, do the next check
+            slti $t4,$t5,113 # check if character <= ascii code for 'p' # the string char in $t5 should be less than or equal to 'p' char i.e. $t4 should be 1
+            beq $t4,$zero,check_A_to_P # if $t4 0 instead of 1, do the next check
 
-        addi $t5,$t5,-87 # convert ascii value to integer (a-p ascii: 97-112; a-p here: 10-25)
-        j add_to_running_sum # j to segment of loop that adds char value to value of $v1, the running sum
+            addi $t5,$t5,-87 # convert ascii value to integer (a-p ascii: 97-112; a-p here: 10-25)
+            j add_to_running_sum # j to segment of loop that adds char value to value of $v1, the running sum
 
-    check_A_to_P:
-        slti $t4,$t5,65 # the string char in $t5 should be greater than or equal to 'A' char i.e. $t4 should be 0
-        bne $t4,$zero,for_non_valid_substrings # if $t4 not 0, do the next check
-        slti $t4,$t5,81 # check if character <= ascii code for 'P' # the string char in $t5 should be less than or equal to 'p' char i.e. $t4 should be 1
-        beq $t4,$zero,for_non_valid_substrings # if $t4 0 instead of 1, do the next check
+        check_A_to_P:
+            slti $t4,$t5,65 # the string char in $t5 should be greater than or equal to 'A' char i.e. $t4 should be 0
+            bne $t4,$zero,for_non_valid_substrings # if $t4 not 0, do the next check
+            slti $t4,$t5,81 # check if character <= ascii code for 'P' # the string char in $t5 should be less than or equal to 'p' char i.e. $t4 should be 1
+            beq $t4,$zero,for_non_valid_substrings # if $t4 0 instead of 1, do the next check
 
-        addi $t5,$t5,-55 # convert ascii value to integer (A-P ascii: 65-80; A-P here: 10-25)
-        j add_to_running_sum # j to segment of loop that adds char value to value of $v1, the running sum
+            addi $t5,$t5,-55 # convert ascii value to integer (A-P ascii: 65-80; A-P here: 10-25)
+            j add_to_running_sum # j to segment of loop that adds char value to value of $v1, the running sum
 
-    for_non_valid_substrings:
-        # check if space char, if not it is invalid. input is invalid
-        li $t4,32 # holds space char ascii value
-        beq $t5,$t4,space_found_after_or_between_valid_chars # if current char is space, update $t3
-        li $t4,9 # holds tab char ascii value
-        beq $t5,$t4,space_found_after_or_between_valid_chars # if current char is space, update $t3
+        for_non_valid_substrings:
+            # check if space char, if not it is invalid. input is invalid
+            li $t4,32 # holds space char ascii value
+            beq $t5,$t4,space_found_after_or_between_valid_chars # if current char is space, update $t3
+            li $t4,9 # holds tab char ascii value
+            beq $t5,$t4,space_found_after_or_between_valid_chars # if current char is space, update $t3
 
-        # any other character is invalid
-        sw $zero,4($sp) # store the validity of the substring as non-valid (0)
+            # any other character is invalid
+            sw $zero,4($sp) # store the validity of the substring as non-valid (0)
+            jr $ra
+
+            space_found_after_or_between_valid_chars:
+                beq $t0,$zero,loop # if no valid chars have been found i.e. space/tab is leading, not sandwiched between valid chars, loop again
+                addi $t3,$t3,1 # if space/tab is after valid character, update t3
+                j sub_b_loop
+
+    end_of_string:
+        # TODO: store in stack the validity of string, the running sum/decimal value, the number of valid chars
+        li $t4,-1
+        sw $t4,16($sp)
+
+    add_to_running_sum:
+        bne $t3,0,for_non_valid_substrings # if spaces/tabs are sandwiched between chars, it is a non-valid input
+        li $t0,1 # valid chars have been found
+        addi $t2,$t2,1 # increment number of valid characters found
+        # check if too many valid chars found (5+)
+        li $t4,5
+        beq $t2,$t4,for_non_valid_substrings
+        li $t4,26
+        mul $t1,$t1,$t4 # multiple previous sum by power of 26 since this converts to base 26 number
+        addu $t1,$t1,$t5 # add current valid digit stored in $t5
+        j sub_b_loop
+
+    exit_sub_b:
+        # TODO: store in stack the validity of string, the running sum/decimal value, the number of valid chars
         jr $ra
-
-        space_found_after_or_between_valid_chars:
-            beq $t0,$zero,loop # if no valid chars have been found i.e. space/tab is leading, not sandwiched between valid chars, loop again
-            addi $t3,$t3,1 # if space/tab is after valid character, update t3
-            j sub_b_loop
-
-end_of_string:
-    # TODO: store in stack the validity of string, the running sum/decimal value, the number of valid chars
-    li $t4,-1
-    sw $t4,16($sp)
-
-add_to_running_sum:
-    bne $t3,0,for_non_valid_substrings # if spaces/tabs are sandwiched between chars, it is a non-valid input
-    li $t0,1 # valid chars have been found
-    addi $t2,$t2,1 # increment number of valid characters found
-    # check if too many valid chars found (5+)
-    li $t4,5
-    beq $t2,$t4,for_non_valid_substrings
-    li $t4,26
-    mul $t1,$t1,$t4 # multiple previous sum by power of 26 since this converts to base 26 number
-    addu $t1,$t1,$t5 # add current valid digit stored in $t5
-    j sub_b_loop
-
-exit_sub_b:
-    # TODO: store in stack the validity of string, the running sum/decimal value, the number of valid chars
-    jr $ra
