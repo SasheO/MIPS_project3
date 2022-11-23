55772fc54028b6432bb90b3690faef8d5b818ddb
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-23 14:34:51 -0500

    included instructions to read input string

--- project3.s
+++ project3.s
@@ -1,7 +1,17 @@
 .data
+input_str: .space 1001
 
 .text
 main:
+# forward slash ascii: 47
+# question mark ascii: 63
+# newline ascii: 10
+
+li $v0,8 # read string
+la $a0,input_str
+li $a1,1001
+syscall 
+
 
 li $v0, 10 # exit program syscall
 syscall
