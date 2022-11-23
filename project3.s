247aaf1fa0f97367657625b139d1f9fe58c21f20
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-23 14:42:59 -0500

    declared subprograms for processing whole sting and each substring

--- project3.s
+++ project3.s
@@ -12,6 +12,22 @@ la $a0,input_str
 li $a1,1001
 syscall 
 
-
 li $v0, 10 # exit program syscall
 syscall
+
+sub_a: # subprogram to process entire input into substrings
+#################################################################
+# comment
+#
+################################################################
+
+jr $ra
+
+
+sub_b: # subprogram to process each substring
+#################################################################
+# comment
+#
+##################################################################
+
+jr $ra
