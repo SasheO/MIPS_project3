39fd83f7c924ffd6cf2b2f2aaf1319e5dccea47c
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-23 14:46:17 -0500

    updated sub_a comments, called sub_a from main
     stored input address in stack in main

--- project3.s
+++ project3.s
@@ -12,13 +12,20 @@ la $a0,input_str
 li $a1,1001
 syscall 
 
+addi $sp,$sp,-4
+sw $a0,0($sp)
+addi $sp,$sp,4
+
+jal sub_a
+
 li $v0, 10 # exit program syscall
 syscall
 
 sub_a: # subprogram to process entire input into substrings
 #################################################################
-# comment
-#
+# sub_a parses the input string and prints out the integers and error messages one by one, with them separated by single comma
+# input used: address of input string from stack
+# outputs: none
 ################################################################
 
 jr $ra
