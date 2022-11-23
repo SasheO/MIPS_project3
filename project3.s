cfbbffd3b234e2734a71e7c1e76a0794bdbf9991
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-23 15:29:05 -0500

    sub_a: stored frame pointed info, included instruction to store current address

--- project3.s
+++ project3.s
@@ -32,6 +32,7 @@ sub_a: # subprogram to process entire input into substrings
 # calls sub_b
 ################################################################
     lw $t0,0($sp) # $t0 contains the address of the string
+    sw $fp,0($sp) # store frame information
 
 
     sub_a_loop:
@@ -43,6 +44,13 @@ sub_a: # subprogram to process entire input into substrings
         li $t2,32 # $t2 contains ascii value of space
         beq $t1,$t2,sub_a_loop # loop again if curren character is space
 
+        addi $sp,$sp,-4
+        add $t0,$t0,-1
+        sw $t0,0($sp)
+        add $t0,$t0,1
+
+
+        sw 
         print_unrecognized_input:
             li $v0,11 # print char
             li $a0,63 # question mark ascii
