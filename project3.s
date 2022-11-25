62cc9a160c96a9bcd7e6d144298150af2b88d2ca
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-24 20:46:02 -0500

    closed sub_a_loop_2

--- project3.s
+++ project3.s
@@ -72,7 +72,7 @@ sub_a: # subprogram to process entire input into substrings
                 # TODO: fill in printing a comma before if it isnt the first substring
 
         sub_a_loop_2:
-            # TODO: reads address from sub_b output (in stack), loops until comma, null char, enter then starts loop again fot next substring
+            # reads address from sub_b output (in stack), loops until comma, null char, enter then starts loop again fot next substring
             lw $t0,0($sp) # $t0 contains the address of the first character of the substring
             lb $t1,0($t0) # load character at this of string into $t1
             addi $t0,$t0,1 # increment by 1 so that $t0 stores address of next character in loop
@@ -81,6 +81,7 @@ sub_a: # subprogram to process entire input into substrings
             li $t2,10 # $t2 contains ascii value of enter/newline
             beq $t1,$t2,sub_a_loop_1 # loop again if curren character is space
             beq $t1,$zero,exit_sub_a # exit loop when you get to the end of the string
+            j sub_a_loop_2
 
 
         j sub_a_loop
